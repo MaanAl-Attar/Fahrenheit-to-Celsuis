d56c776caf62f92d0d3fa99adb08e43ac6d84fe9
MaanAl-Attar <m.al-attar@share.epsb.ca>
2024-11-28 14:34:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("cal-btn").addEventListener("click", Calculate);
+
+function Calculate() {
+  let fahernheit = document.getElementById("F-input").value;
+
+    let answer = (( fahernheit - 32 ) * (5/9));
+
+  document.getElementById("F-input").value = " "
+
+  answer = answer.toFixed(0);
+  
+  document.getElementById("output").innerHTML = answer;
+  document.getElementById("output").style.border = "2px solid #26d978"
+
+  document.getElementById("pic-container").innerHTML = "<img src ='theremometer.jpg'>"
+}
+  
+
